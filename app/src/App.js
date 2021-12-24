bd8633e39625b7145fbc1d49c5bd9f5ea1bf946b
James J Gault <74686199+JJG1488@users.noreply.github.com>
2021-12-24 01:50:24 -0500

    added link back

--- app/src/App.js
+++ app/src/App.js
@@ -4,8 +4,8 @@ import twitterLogo from './assets/twitter-logo.svg';
 import CandyMachine from './CandyMachine';
 
 // Constants
-// const TWITTER_HANDLE = '_buildspace';
-// const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   // State
@@ -77,12 +77,12 @@ const App = () => {
         {walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          {/* <a
+          <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a> */}
+          >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
